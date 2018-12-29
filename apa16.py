2d8452ed4f6f005d376ff246eb076c446b6180b3
aparajitakundu <46236437+aparajitakundu@users.noreply.github.com>
2018-12-29 14:16:02 +0530

    Add files via upload

---
+++ apa16.py
@@ -0,0 +1,12 @@
+#question 19 done by aparajita kundu
+a=int(input("enter the first side of a triangle"))
+b=int(input("enter the second side of the triangle"))
+c=int(input("enter the third side of the triangle"))
+if (a+b>c):
+    print("triangle is valid")
+elif (b+c>a):
+    print("triangle is valid")
+elif (c+a>b):
+    print("triangle is valid")
+else:
+    print("triangle is not valid")
