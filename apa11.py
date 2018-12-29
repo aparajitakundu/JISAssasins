324ced35483b8ad422165e00f667e59c9a83f280
aparajitakundu <46236437+aparajitakundu@users.noreply.github.com>
2018-12-29 13:55:48 +0530

    Add files via upload

---
+++ apa11.py
@@ -0,0 +1,10 @@
+#question 11 done by aparajita kundu
+print("Enter '0' for exit.");
+ch = input("Enter any character: ");
+if ch == '0':
+    exit();
+else:
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	print(ch, "is an alphabet.");
+    else:
+    	print(ch, "is not an alphabet.");
