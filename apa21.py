11dd7dff33b367f41f4bd92e8b565f72e89db4c2
aparajitakundu <46236437+aparajitakundu@users.noreply.github.com>
2018-12-29 15:45:09 +0530

    Add files via upload

---
+++ apa21.py
@@ -0,0 +1,12 @@
+#question 17 done by aparajita kundu
+x= input('enter the set of numbers :(enter space seperatedvalue):')
+list1= x.split(" ")
+res=[]
+for i in list1:
+    n = int(i)
+    for j in range(2,int(n/2)):
+        if(n%j == 0):
+            break
+    else:
+        res.append(n)
+print(res)
