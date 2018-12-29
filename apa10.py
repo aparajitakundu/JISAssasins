6e6fe9be9260e98e8aabfd3ff3cb5b07b918c198
aparajitakundu <46236437+aparajitakundu@users.noreply.github.com>
2018-12-29 12:50:49 +0530

    Add files via upload

---
+++ apa10.py
@@ -0,0 +1,5 @@
+str=input("Enter the string")
+char=input("Enter the character whose number of occurences is to be found in the string")
+c=str.count(char)
+print("Number of occurences of {} is {}".format(char,c))
+
