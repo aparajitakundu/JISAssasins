c019558ebd9bdc77d5b25a6784c2db9c27b07ed7
aparajitakundu <46236437+aparajitakundu@users.noreply.github.com>
2018-12-29 15:48:21 +0530

    Add files via upload

---
+++ apa22.py
@@ -0,0 +1,15 @@
+#question 23 done by aparajita kundu
+X = input("Enter your string")
+l=len(X)
+print("The length is", l)
+Y = "Ankita"
+Z= X+Y
+print("After concatenation", Z)
+print(''.join(reversed(X)))
+old="I like Python"
+new=old.replace("like","love")
+print(new)
+if X == Y:
+    print("Equal")
+else:
+    print("Not Equal")
